e27c16088f69d1d1e6495e1f9f0f8acc75489864
Alan Islas <alan.islas.itic@gmail.com>
2021-05-06 11:05:45 -0500

    List added

--- todos.js
+++ todos.js
@@ -1,8 +1,15 @@
 import { LitElement, html } from 'lit-element';
+import '@material/mwc-list';
 
 class TodoList extends LitElement {
   render() {
-    return html` <div>Hello from MyElement!</div> `;
+    return html`
+      <mwc-list>
+        <mwc-check-list-item selected>Item 0</mwc-check-list-item>
+        <mwc-check-list-item selected>Item 0</mwc-check-list-item>
+        <mwc-check-list-item selected>Item 0</mwc-check-list-item>
+      </mwc-list>
+    `;
   }
 }
 
