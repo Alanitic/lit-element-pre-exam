e9584c7074ec61fb5844a60a4e49ca6d6e636dc9
Alan Islas <alan.islas.itic@gmail.com>
2021-05-06 11:38:28 -0500

    Template completed

--- todos.js
+++ todos.js
@@ -1,14 +1,28 @@
-import { LitElement, html } from 'lit-element';
+import { LitElement, html, css } from 'lit-element';
+import '@material/mwc-button';
 import '@material/mwc-list';
+import '@material/mwc-icon';
+import '@material/mwc-textfield';
+import '@material/mwc-list/mwc-check-list-item';
 
 class TodoList extends LitElement {
   render() {
     return html`
-      <mwc-list>
-        <mwc-check-list-item selected>Item 0</mwc-check-list-item>
-        <mwc-check-list-item selected>Item 0</mwc-check-list-item>
-        <mwc-check-list-item selected>Item 0</mwc-check-list-item>
+      <mwc-textfield outlined label="Tarea"></mwc-textfield>
+      <mwc-button outlined label="Agregar"></mwc-button>
+      <mwc-list multi id="checklist">
+        <mwc-check-list-item graphic="avatar" selected
+          >Item 0
+          <mwc-icon slot="graphic">delete</mwc-icon></mwc-check-list-item
+        >
       </mwc-list>
+    `;
+  }
+  static get styles() {
+    return css`
+      div {
+        color: red;
+      }
     `;
   }
 }
